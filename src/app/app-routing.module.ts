92d06c480ab10d478e66f5afe6a27f1ef2acecf0
Marjory Marquez <samaterra@hotmail.com>
2023-01-22 11:22:22 -0500

    nodemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { RegistrationComponent } from './registration/registration.component';
-
+import { Routes, RouterModule } from '@angular/router';
+import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
 const routes: Routes = [
-  {
-  path: '',
-  component: HomeComponent
-  },
-  {
-  path: 'registration',
-  component: RegistrationComponent
-  }
-  ];
-
+  { path: '', pathMatch: 'full', redirectTo: 'create-employee' },
+  { path: 'create-employee', component: EmployeeCreateComponent },
+  { path: 'edit-employee/:id', component: EmployeeEditComponent },
+  { path: 'employees-list', component: EmployeeListComponent }  
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
-import { NgbDateStruct, NgbDateAdapter, NgbDateNativeUTCAdapter} from '@ng-bootstrap/ng-bootstrap';
 import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
 import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    RegistrationComponent,
     EmployeeCreateComponent,
     EmployeeEditComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     FormsModule,
-    NgbModule
+    HttpClientModule,
   ],
-  providers: [
-    [{provide: NgbDateAdapter, useClass: NgbDateNativeUTCAdapter}]
-  ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
