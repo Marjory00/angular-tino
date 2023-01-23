8516744566a802c6e26c29c65709b4b30a9e41e2
Marjory Marquez <samaterra@hotmail.com>
2023-01-23 04:03:56 -0500

    a

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './../../service/api.service';
+import { Employee } from 'src/app/model/employee';
+import { EmployeesService } from 'src/app/service/employees.service';
+
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
+
 export class EmployeeListComponent implements OnInit {
   
+  employees : Employee[] = [];
+
+
   Employee:any = [];
-  constructor(private apiService: ApiService) { 
+
+  constructor(private apiService: ApiService,
+    private employeesService: EmployeesService ) { 
+
     this.readEmployee();
   }
+  
   ngOnInit() {}
   readEmployee(){
     this.apiService.getEmployees().subscribe((data) => {
